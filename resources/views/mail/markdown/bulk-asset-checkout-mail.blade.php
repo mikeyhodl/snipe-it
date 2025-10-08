9bdd0d1d1e76c4ad3912f8b754b778dc358624c8
Marcus Moore <contact@marcusmoore.io>
2025-10-08 16:05:55 -0700

    Add admin name

--- resources/views/mail/markdown/bulk-asset-checkout-mail.blade.php
+++ resources/views/mail/markdown/bulk-asset-checkout-mail.blade.php
@@ -3,9 +3,10 @@
 
 {{ $introduction }}
 
+**{{ trans('general.administrator') }}**: {{ $admin->display_name }}
+
 @if ($requires_acceptance == 1)
 One or more items require acceptance.<br>
-
 **[✔ Click here to review the terms of use and accept the items]({{ $acceptance_url }})**
 @endif
 
