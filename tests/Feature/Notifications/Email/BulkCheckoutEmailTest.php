33c156be160498e212393a14fb1abf8963b4b461
Marcus Moore <contact@marcusmoore.io>
2025-10-21 12:43:23 -0700

    Add failing test

--- tests/Feature/Notifications/Email/BulkCheckoutEmailTest.php
+++ tests/Feature/Notifications/Email/BulkCheckoutEmailTest.php
@@ -4,14 +4,32 @@ namespace Tests\Feature\Notifications\Email;
 
 use App\Events\CheckoutablesCheckedOutInBulk;
 use App\Mail\BulkAssetCheckoutMail;
+use App\Mail\CheckoutAssetMail;
 use App\Models\Asset;
 use App\Models\User;
-use App\Notifications\CheckoutAssetNotification;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class BulkCheckoutEmailTest extends TestCase
 {
+    private $assets;
+    private $target;
+    private $admin;
+    private $checkout_at;
+    private $expected_checkin;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->assets = Asset::factory()->count(2)->create();
+        $this->target = User::factory()->create(['email' => 'someone@example.com']);
+        $this->admin = User::factory()->create();
+        $this->checkout_at = date('Y-m-d H:i:s');
+        $this->expected_checkin = '';
+    }
+
+    // @todo:
     public static function scenarios()
     {
         // 'User has email address set
@@ -20,39 +38,43 @@ class BulkCheckoutEmailTest extends TestCase
         // 'webhook is set'
     }
 
-    public function test_email_is_sent()
+    public function test_email_is_sent_to_cc_address()
     {
         $this->settings->enableAdminCC('cc@example.com');
 
         Mail::fake();
 
-        $assets = Asset::factory()->count(2)->create();
-        $target = User::factory()->create(['email' => 'someone@example.com']);
-        $admin = User::factory()->create();
-        $checkout_at = date('Y-m-d H:i:s');
-        $expected_checkin = '';
+        $this->dispatchEvent();
 
-        CheckoutablesCheckedOutInBulk::dispatch(
-            $assets,
-            $target,
-            $admin,
-            $checkout_at,
-            $expected_checkin,
-            'A note here',
-        );
-
-        Mail::assertNotSent(CheckoutAssetNotification::class);
+        Mail::assertNotSent(CheckoutAssetMail::class);
 
         Mail::assertSent(BulkAssetCheckoutMail::class, 2);
 
         Mail::assertSent(BulkAssetCheckoutMail::class, function (BulkAssetCheckoutMail $mail) {
             // @todo: assert contents
-            return $mail->hasTo('someone@example.com');
+            return $mail->hasTo($this->target->email);
         });
 
         Mail::assertSent(BulkAssetCheckoutMail::class, function (BulkAssetCheckoutMail $mail) {
             // @todo: assert contents
             return $mail->hasTo('cc@example.com');
         });
+    }
+
+    public function test_webbook_is_sent()
+    {
+        $this->markTestIncomplete();
+    }
+
+    private function dispatchEvent(): void
+    {
+        CheckoutablesCheckedOutInBulk::dispatch(
+            $this->assets,
+            $this->target,
+            $this->admin,
+            $this->checkout_at,
+            $this->expected_checkin,
+            'A note here',
+        );
     }
 }
