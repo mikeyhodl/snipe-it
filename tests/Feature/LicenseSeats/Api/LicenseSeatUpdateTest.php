e25e405f7490756ba8f75e265a367830a5a5720d
Marcus Moore <contact@marcusmoore.io>
2026-02-18 14:52:43 -0800

    Scaffold additional test cases and organize

--- tests/Feature/LicenseSeats/Api/LicenseSeatUpdateTest.php
+++ tests/Feature/LicenseSeats/Api/LicenseSeatUpdateTest.php
@@ -18,72 +18,6 @@ class LicenseSeatUpdateTest extends TestCase
             ->assertForbidden();
     }
 
-    public function test_license_seat_can_be_updated()
-    {
-        $this->markTestIncomplete();
-    }
-
-    public function test_license_seat_can_be_checked_out_when_updating()
-    {
-        $this->markTestIncomplete();
-
-        $license = License::factory()->create();
-        $licenseSeat = LicenseSeat::factory()->for($license)->create([
-            'assigned_to' => null,
-        ]);
-
-        $targetUser = User::factory()->create();
-
-        $payload = [
-            'assigned_to' => $targetUser->id,
-            'notes' => 'Checking out the seat to a user',
-        ];
-
-        $response = $this->actingAsForApi(User::factory()->superuser()->create())
-            ->patchJson(
-                $this->route($licenseSeat),
-                $payload
-            );
-
-        $response->assertStatus(200)
-            ->assertJsonFragment([
-                'status' => 'success',
-            ]);
-
-        $licenseSeat->refresh();
-
-        $this->assertEquals($targetUser->id, $licenseSeat->assigned_to);
-        $this->assertEquals('Checking out the seat to a user', $licenseSeat->notes);
-        $this->assertHasTheseActionLogs($license, ['add seats', 'create', 'checkout']); //FIXME - backwards
-    }
-
-    public function test_license_seat_can_be_checked_in_when_updating()
-    {
-        $this->markTestIncomplete();
-
-        $licenseSeat = LicenseSeat::factory()->reassignable()->assignedToUser()->create();
-
-        $this->actingAsForApi(User::factory()->superuser()->create())
-            ->patchJson(
-                route('api.licenses.seats.update', [$licenseSeat->license->id, $licenseSeat->id]),
-                [
-                    'assigned_to' => null,
-                    'notes' => '',
-                ]
-            )
-            ->assertStatus(200)
-            ->assertStatusMessageIs('error');
-
-        $licenseSeat->refresh();
-
-        $this->assertNull($licenseSeat->assigned_to);
-    }
-
-    public function test_cannot_reassign_unreassignable_license_seat()
-    {
-        $this->markTestIncomplete();
-    }
-
     public function test_assigned_to_cannot_be_array()
     {
         $licenseSeat = LicenseSeat::factory()->create(['assigned_to' => null]);
@@ -123,6 +57,108 @@ class LicenseSeatUpdateTest extends TestCase
             ->assertStatus(200)
             ->assertStatusMessageIs('error')
             ->assertMessagesContains('assigned_to');
+    }
+
+    public function test_assigned_to_and_asset_id_cannot_be_provided_together()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_license_seat_can_be_updated()
+    {
+        $licenseSeat = LicenseSeat::factory()->create();
+
+        $this->actingAsForApi(User::factory()->superuser()->create())
+            ->patchJson($this->route($licenseSeat), [
+                'notes' => 'A new note is here',
+            ])
+            ->assertStatus(200)
+            ->assertStatusMessageIs('success');
+
+        $licenseSeat->refresh();
+
+        $this->assertEquals('A new note is here', $licenseSeat->notes);
+    }
+
+    public function test_created_by_and_timestamps_are_not_updated()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_reassignableness_cannot_be_updated()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_license_seat_can_be_checked_out_to_user_when_updating()
+    {
+        $this->markTestIncomplete();
+
+        $license = License::factory()->create();
+        $licenseSeat = LicenseSeat::factory()->for($license)->create([
+            'assigned_to' => null,
+        ]);
+
+        $targetUser = User::factory()->create();
+
+        $payload = [
+            'assigned_to' => $targetUser->id,
+            'notes' => 'Checking out the seat to a user',
+        ];
+
+        $response = $this->actingAsForApi(User::factory()->superuser()->create())
+            ->patchJson(
+                $this->route($licenseSeat),
+                $payload
+            );
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'status' => 'success',
+            ]);
+
+        $licenseSeat->refresh();
+
+        $this->assertEquals($targetUser->id, $licenseSeat->assigned_to);
+        $this->assertEquals('Checking out the seat to a user', $licenseSeat->notes);
+        $this->assertHasTheseActionLogs($license, ['add seats', 'create', 'checkout']); //FIXME - backwards
+    }
+
+    public function test_license_seat_can_be_checked_out_to_asset_when_updating()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_license_seat_can_be_checked_in_when_updating()
+    {
+        $this->markTestIncomplete();
+
+        $licenseSeat = LicenseSeat::factory()->reassignable()->assignedToUser()->create();
+
+        $this->actingAsForApi(User::factory()->superuser()->create())
+            ->patchJson(
+                route('api.licenses.seats.update', [$licenseSeat->license->id, $licenseSeat->id]),
+                [
+                    'assigned_to' => null,
+                    'notes' => '',
+                ]
+            )
+            ->assertStatus(200)
+            ->assertStatusMessageIs('error');
+
+        $licenseSeat->refresh();
+
+        $this->assertNull($licenseSeat->assigned_to);
+    }
+
+    public function test_cannot_change_license_for_license_seat()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_cannot_reassign_unreassignable_license_seat()
+    {
+        $this->markTestIncomplete();
     }
 
     private function route(LicenseSeat $licenseSeat)
